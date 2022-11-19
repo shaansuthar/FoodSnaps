de9cde7d99675ec28ee884491502fe578549d8dc
shaansuthar <shaan.suthar@icloud.com>
2022-11-19 10:59:28 -0500

    Update file structure

---
+++ nutrient-ai-identifier/types.tsx
@@ -0,0 +1,32 @@
+export type RootStackParamList = {
+  Root: undefined;
+  NotFound: undefined;
+};
+
+export type BottomTabParamList = {
+  Home: undefined;
+  ClassifyImage: undefined;
+  DetectObjects: undefined;
+  DetectFoods: undefined;
+  References: undefined;
+};
+
+export type HomeParamList = {
+  HomeScreen: undefined;
+};
+
+export type ClassifyImageParamList = {
+  ClassifyImageScreen: undefined;
+};
+
+export type DetectObjectsParamList = {
+  DetectObjectsScreen: undefined;
+};
+
+export type DetectFoodsParamList = {
+  DetectFoodScreen: undefined;
+};
+
+export type ReferencesParamList = {
+  ReferencesScreen: undefined;
+};
