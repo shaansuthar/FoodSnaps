2181f494e67bc620567c8c887267512cadf7fdec
richardl2003 <richardliy03@gmail.com>
2022-12-28 17:34:14 -0500

    Displayed the recipes in the scrollable order

--- nutrient-ai-identifier/screens/DetectFoodsScreen.tsx
+++ nutrient-ai-identifier/screens/DetectFoodsScreen.tsx
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { View, Button, Image, StyleSheet, Text } from "react-native";
+import { View, Button, Image, StyleSheet, Text, ScrollView } from "react-native";
 import * as ImagePicker from "expo-image-picker";
 import {SPOONACULAR_API_KEY} from "@env";
 import Axios from "axios";
+
+const recipe_width = '40%'
+const recipe_img_width = '30%'
+const recipe_img_height = '40%'
 
 const createFormData = (uri) => {
   const fileName = uri.split('/').pop();
@@ -38,46 +42,30 @@ export default function DetectFoodsScreen() {
   }
 
   const getData = async() => {
-    const formData = createFormData(image)     
+    const formData = createFormData(image)
+    setUploaded(true)     
     Axios.post(`https://api.spoonacular.com/food/images/analyze?apiKey=${SPOONACULAR_API_KEY}`, formData)
-    .then(response => {setName(response.data.category.name); setRecipes(response.data.recipes); setUploaded(true); console.log(response.data)})
+    .then(response => {setName(response.data.category.name); setRecipes(response.data.recipes); console.log(response.data)})
   }
 
   if (image != null) {
+
     return (
-      <View style={styles.container}>
+      <ScrollView>
         <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
-        <Button title="Get Recipe and Nutritional Information" onPress={getData} />
-      </View>
-    )
-  }
-
-  if (name != null && recipes != null && uploaded === true) {
-    console.log('name is: ' + name)
-
-    return (
-      <View>
-        <Text style={{fontSize: 20, fontWeight: 'bold'}}>The Recognized Food is: {name}</Text>
-        <div className="recipes" style={styles.recipes}>
+        <Button title="Get Recipe and Nutritional Information" onPress={getData} disabled={uploaded}/>
+        <View>
         {recipes &&
           recipes.map((recipe, index) => {
-
             return (
-              <div className="recipe" key={index} style={styles.recipe}>
-                <h3>Recipe {index + 1}</h3>
-                <h2>{recipe.name}</h2>
-
-                <div className="details" style={styles.details}>
-                  <Text>{recipe.name}</Text>
-                  <Image source={{ uri: recipe.image }} style={{ width: 20, height: 20 }} />
-                  <Text><a href={recipe.url}>Link to Recipe</a></Text>
-                </div>
-              </div>
+              <View key={index} style={styles.recipe}>
+                <Text>{recipe.title}</Text>
+                <Image source={{uri: `https://spoonacular.com/recipeImages/${recipe.id}-240x150.${recipe.imageType}`}} style={{width: 100, height: 100}}/>
+              </View>
             );
           })}
-        </div>
-
-      </View>
+        </View>        
+      </ScrollView>
     )
   }
 
@@ -95,22 +83,17 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   recipes: {
-    display: "flex",
-    flexWrap: "wrap",
+    display: "flex"
   },
   recipe: {
-    position: "relative",
+    maxWidth: recipe_width,
     borderRadius: 5,
     backgroundColor: "#05d31d",
-    flexBasis: "width",
     color: "#000000",
-    padding: "20px",
-    margin: "10px",
-    width: "40%",
+    padding: 20 ,
+    margin: 10,
+    width: recipe_width,
   },
-  details: {
-    textAlign: "left",
-  }
 });
 
 
