a4cd201938fba5a17cfffd7a7efe6aeed520658a
richardl2003 <richardliy03@gmail.com>
2023-01-04 13:54:33 -0500

    Able to display responses, need to fix _id overlap issue to have a continuous conversation

--- nutrient-ai-identifier/screens/ChatbotScreen.tsx
+++ nutrient-ai-identifier/screens/ChatbotScreen.tsx
@@ -1,93 +1,86 @@
-import React, { Component } from 'react'
-import { View, StyleSheet } from 'react-native'
+import React, { useState, useCallback, useEffect } from 'react'
+import { View, StyleSheet, ViewPagerAndroidComponent } from 'react-native'
 import { GiftedChat } from 'react-native-gifted-chat'
-const API_URL = "https://3c86-2001-1970-59e1-8a00-5922-39be-f382-9d6c.ngrok.io/api/"
+const API_URL = "https://729f-2001-1970-59e1-8a00-38fd-729b-e62b-7e18.ngrok.io/api/"
  
 const botAvatar = require('../assets/images/foodsnaps_logo.png')
+
 const BOT = {
   _id: 2,
   name: 'Sprite',
   avatar: botAvatar
 }
 
-class ChatbotScreen extends Component {
-  state = {
-      messages: [
-        {_id: 2, text: "Enter ingredients that you want to cook with and I'll provide a recipes for you!", createdAt: new Date(), user: BOT},
-        {_id: 1, text: 'Hi!', createdAt: new Date(), user: BOT}
-      ],
-      id: 1,
-      name: ''
-  }
+const introMessage = {
+    _id: 1,
+    text: "Hi! I am a recipe bot. I can help you find recipes based on ingredients. Try it now!",
+    createdAt: new Date(),
+    user: BOT,
+} 
 
-  sendOpenaiResponse = (response) => {
-    console.log(response)
+export default function ChatBotScreen() {
 
-    let msg = {
-        _id: this.state.messages.length + 1,
-        text: response,
-        createdAt: new Date(),
-        user: BOT
-    }
+    const [messages, setMessages] = useState([])
 
-    this.setState((previous) => {
-        messages: GiftedChat.append(previous.messages, [msg])
-    })
-  }
- 
-   onSend(messages = []) {
-       this.setState((previous) => {
-           messages: GiftedChat.append(previous.messages, messages)
-       })
+    useEffect(() => {
+        setMessages([introMessage])
+      }, [])
 
-       this.state.messages
- 
-       let prompt = messages[0].text
-       var myHeaders = new Headers();
-       myHeaders.append("Content-Type", "application/json");
-       myHeaders.append("Accept", "application/json");
-       
-       var raw = JSON.stringify({
-         "ingredient": prompt
-       });
-       
-       var requestOptions = {
-         method: 'POST',
-         headers: myHeaders,
-         body: raw,
-         redirect: 'follow'
-       };
-       
-       fetch(`${API_URL}generateRecipes`, requestOptions)
-         .then(response => response.json())
-         .then(result => {
-           console.log(result.result)
-           this.sendOpenaiResponse(result.result)
-         })
-         .catch(error => console.log('error', error));    
-  }
- 
-  render() {
-      return(
+      const onSend = useCallback((messages = []) => {
+        setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
+        let prompt = messages[0].text
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+        
+        var raw = JSON.stringify({
+          "ingredient": prompt
+        });
+        
+        var requestOptions = {
+          method: 'POST',
+          headers: myHeaders,
+          body: raw,
+          redirect: 'follow'
+        };
+        
+        fetch("https://729f-2001-1970-59e1-8a00-38fd-729b-e62b-7e18.ngrok.io/api/generateRecipes", requestOptions)
+          .then(response => response.json())
+          .then(result => {
+            handleOpenAIResponse(result)
+          })
+          .catch(error => console.log('error', error));        
+
+        
+      }, [])
+
+      const handleOpenAIResponse = (response) => {
+        let text = response.result.trim()
+        console.log(messages.length)
+        let msg = {
+            _id: messages.length + 2,
+            text: text,
+            createdAt: new Date(),
+            user: BOT,
+        }
+
+        setMessages(previousMessages => GiftedChat.append(previousMessages, [msg]))
+      }
+
+      return (
         <View style={styles.container}>
             <GiftedChat 
-            messages={this.state.messages}
-            onSend={(messages) => this.onSend(messages)}
+            messages={messages}
+            onSend={messages => onSend(messages)}
             user={{_id: 1}}
-            bottomOffset={74}
-            alignTop={true}
-            infiniteScroll={true}
-            scrollToBottom
-            />            
+            bottomOffset={74} />
         </View>
       )
-  }
 }
+
 const styles = StyleSheet.create({
-  container: {
-   flex: 1,
-   marginBottom: -36,
-   backgroundColor: '#FFF'
-  },
-});
-export default ChatbotScreen
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        marginBottom: -38,
+    }
+})
